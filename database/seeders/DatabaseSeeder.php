6ac5832d8360e1a25e4fbeba77b0cdca0e9c47de
zildanfaisal <zetisal.mz@gmail.com>
2025-12-20 16:35:05 +0700

    disable customer seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             UserSeeder::class,
-            CustomerSeeder::class,
+            // CustomerSeeder::class,
         ]);
     }
 }
